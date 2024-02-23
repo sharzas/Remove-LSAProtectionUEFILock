df017af6adc7f385f5a4833382d8d92c1d7ec2af
Kenneth Nielsen <ken@jndata.dk>
2024-02-23 15:08:17 +0100

    Updated batch script

--- Install-LSAProtection-UEFI-OptOut-BootLoader.cmd
+++ Install-LSAProtection-UEFI-OptOut-BootLoader.cmd
@@ -1,14 +1,28 @@
 @echo off
+:: Test status of RunAsPpl
+SET __RunAsPPL=0
+
+reg query hklm\system\currentcontrolset\control\lsa /v RunAsPpl|find /i "RunAsPpl"|find /i "0x1" > nul
+if %errorlevel% == 0 SET __RunAsPPL=1
+
+reg query hklm\system\currentcontrolset\control\lsa /v RunAsPpl|find /i "RunAsPpl"|find /i "0x2" > nul
+if %errorlevel% == 0 SET __RunAsPPL=1
+
+:: LSA Protection enabled in registry - must be removed first!
+if %__RunAsPPL%==1 goto RunAsPPLEnabled
+
+
 :: Test if boot entry already added
 bcdedit /enum "{0cb3b571-2f2e-4343-a879-d86a476d7215}" | find /i "{0cb3b571-2f2e-4343-a879-d86a476d7215}" > nul
 if %errorlevel% == 0 SET __RunAsPPLBootLoader=1
 if %errorlevel% == 1 SET __RunAsPPLBootLoader=0
 
 if %__RunAsPPLBootLoader%==0 (if NOT exist %~dp0LSAPPLConfig.efi goto NoBootLoader)
-if %__RunAsPPLBootLoader%==0 if exist x:\ goto XAlreadyMounted
+if %__RunAsPPLBootLoader%==0 (if exist x:\ goto XAlreadyMounted)
 
 if %__RunAsPPLBootLoader%==0 echo Boot loader will be installed.
 if %__RunAsPPLBootLoader%==1 echo Boot loader already installed - will be re-activated.
+echo .
 
 :: Add Opt Out Boot loader to system partition and boot loader table.
 if %__RunAsPPLBootLoader%==0 mountvol X: /s
@@ -34,4 +48,19 @@ goto:eof
 
 :XAlreadyMounted
 echo X: already mounted. X: must be available. Unmount it first then re-run.
+goto:eof
+
+:RunAsPPLEnabled
+echo .
+echo Opt Out Tool Boot loader NOT installed!!
+echo .
+echo HKLM\System\CurrentControlSet\Control\LSA\RunAsPpl is still set to:
+echo .
+reg query hklm\system\currentcontrolset\control\lsa /v RunAsPpl
+echo .
+echo UEFI variable would be set again immediately after boot - remove this setting or set it
+echo to 0x0 to disable RunAsPpl in registry, then rerun this script to install opt out tool!
+echo .
+echo IMPORTANT: Setting may come from a Group Policy - ensure you change that as well!
+echo .
 goto:eof
